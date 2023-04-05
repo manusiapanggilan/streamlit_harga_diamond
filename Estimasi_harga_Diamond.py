b2e2e356636dbcb4ac06a6e08fb7e23d4f9ef18f
Muhammad Ziyan Marzuqi <100933933+manusiapanggilan@users.noreply.github.com>
2023-04-06 00:48:23 +0700

    Add files via upload

---
+++ Estimasi_harga_Diamond.py
@@ -0,0 +1,29 @@
+import pickle
+import streamlit as st
+from PIL import Image
+
+image = Image.open('Diamond.jpg')
+
+st.image(image, caption=' ')
+
+diamond_model = pickle.load(open('Estimasi_harga_diamond.sav','rb'))
+
+st.title('Estimasi Harga Diamond')
+st.caption(' Muhammad Ziyan Marzuqi | 191351178')
+
+
+carat = st.number_input ('Input Carat Berlian')
+depth = st.number_input('Input Depth Berlian')
+table = st.number_input('Input Table Berlian')
+x = st.number_input('Input Panjang (mm)')
+y = st.number_input('Input Lebar (mm)')
+z = st.number_input('Input Kedalaman (mm)')
+
+predict = ''
+
+if st.button('Estimasi Harga Belian') :
+    predict = diamond_model.predict([[carat,depth,table,x,y,z]])
+
+    st.write('Estimasi Harga Diamond dalam USD :', predict)
+    st.write('Estimasi Harga Diamond dalam IDR :', predict*14964)
+    
